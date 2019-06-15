ca4e30bff0f6e6429df6fddb9db6440201ab365f
Kjip <kjipgamer@gmail.com>
2019-06-15 19:42:06 +0200

    Added floor fade in

---
+++ Covel/Assets/FloorFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorFade : MonoBehaviour
+{
+    [SerializeField] float fadePerSecond;
+    Material material;
+    Color color;
+
+    #region Methods
+    
+
+    void start(){
+        color.a = 0f;
+    }
+ 
+     private void Update() {
+        color = GetComponent<Renderer>().material.color;
+         GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, color.a + fadePerSecond * Time.deltaTime);
+         if(Input.GetKeyDown(KeyCode.Space)){
+             GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, 0f);
+         }
+     }
+ 
+
+    #endregion
+}
