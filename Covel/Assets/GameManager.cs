49c41f5eb239dd0c92639c740b37412cc38b4138
Kjip <kjipgamer@gmail.com>
2019-06-19 20:35:59 +0200

    added gamemanager

---
+++ Covel/Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public float floorHeight;
+    public float floorSpeed;
+    public float floorSpawnSpeed; //Time between platform spawns
+    
+}
