92735dbc0711c4f68f4f4f9a145d849ba30997a0
Kjip <kjipgamer@gmail.com>
2019-06-26 20:58:09 +0200

    Added basic movement to player

---
+++ Covel/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float playerMoveSpeed;
+
+
+    Rigidbody playerRb;
+    float horizontal;
+    float vertical;
+
+    #region Methods
+
+    private void Start() {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+
+        playerRb.velocity = new Vector3(horizontal * playerMoveSpeed, playerRb.velocity.y, vertical * playerMoveSpeed);
+         
+
+    }
+
+    #endregion
+}
